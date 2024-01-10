30c580d415448d51878fe8510777a40d2b933e91
Ahmet Zeren <sinanahmetzeren@gmail.com>
2024-01-10 13:37:19 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+/* eslint-disable no-undef */
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to parrots App</Text>
+      <Image
+        style={styles.logo}
+        source={require("./assets/parrots-logo.jpg")}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    height: 150,
+    width: 150,
   },
 });
