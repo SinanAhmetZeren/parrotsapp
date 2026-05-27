76d6a9533f127815a0a2ae1f7a77feb9b86b31c7
SinanAhmetZeren <SinanAhmetZeren@gmail.com>
2026-05-27 16:23:36 +0300

    Bump version to 1.0.11 (versionCode 12)

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
         "name": "Parrots",
         "slug": "parrots",
         "owner": "ahmetzeren",
-        "version": "1.0.10",
+        "version": "1.0.11",
         "scheme": "parrotsapp",
         "orientation": "portrait",
         "icon": "./assets/parrotslogoblue_icon.png",
@@ -41,7 +41,7 @@ export default {
         "android": {
             "package": "com.zenforest.parrots",
             "googleServicesFile": "./google-services.json",
-            "versionCode": 11,
+            "versionCode": 12,
             "intentFilters": [
                 {
                     "action": "VIEW",
