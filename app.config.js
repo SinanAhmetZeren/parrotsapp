eadc759095a88d18315f61a5ad2e6750fe8ef334
SinanAhmetZeren <SinanAhmetZeren@gmail.com>
2026-08-08 22:10:28 +0300

    Bump version to 1.0.26

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
         "name": "Parrots",
         "slug": "parrots",
         "owner": "ahmetzeren",
-        "version": "1.0.25",
+        "version": "1.0.26",
         "scheme": "parrotsapp",
         "orientation": "portrait",
         "icon": "./assets/parrotslogoblue_icon.png",
@@ -42,7 +42,7 @@ export default {
             "softwareKeyboardLayoutMode": "resize",
             "package": "com.zenforest.parrots",
             "googleServicesFile": "./google-services.json",
-            "versionCode": 25,
+            "versionCode": 26,
             "intentFilters": [
                 {
                     "action": "VIEW",
